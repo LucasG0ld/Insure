8c44195ecd4eb10730cde1917ab676cdcfdc7fe7
LucasG0ld <72749416+LucasG0ld@users.noreply.github.com>
2022-03-30 17:18:43 +0200

    Responsive done

--- script/burger.js
+++ script/burger.js
@@ -1,11 +1,17 @@
 /* Sélection des éléments HTML */
-let link = document.getElementById('ins-menu-link')
-let burger = document.getElementById('ins-menu-burger')
-let ul = document.getElementById('ins-burger-ul')
+let link = document.getElementById('ins-menu-link');
+let burger = document.getElementById('ins-menu-burger');
+let div = document.getElementById('ins-burger-container');
+let content = document.getElementById('ins-content');
+let footer = document.getElementById('ins-footer');
+
 
 /* gestionnaire d'événement sur le a#link pour venir changer l'attribution de la classe .open à la ul et au span#burger */
 link.addEventListener('click', function(e) {
   e.preventDefault()
   burger.classList.toggle('open')
-  ul.classList.toggle('open')
+  div.classList.toggle('open')
+  content.classList.toggle('dp-none')
+  footer.classList.toggle('dp-none')
+
 })
